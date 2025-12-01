c2251ba7f76a03e6bb4ea17e924169a890abab75
Dandi2k8 <drd.construction30@gmail.com>
2025-12-01 09:59:49 -0800

    Fixing the crash problem

--- src/main/java/com/drd/mysticaladaptations/MysticalAdaptations.java
+++ src/main/java/com/drd/mysticaladaptations/MysticalAdaptations.java
@@ -9,9 +9,6 @@ import com.drd.mysticaladaptations.init.ModCreativeModeTabs;
 import com.drd.mysticaladaptations.init.ModItems;
 import net.minecraft.client.renderer.item.ItemProperties;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.world.item.crafting.Recipe;
-import net.minecraft.world.item.crafting.RecipeManager;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -23,7 +20,6 @@ import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
-import net.neoforged.neoforge.event.server.ServerStartedEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
@@ -67,47 +63,6 @@ public class MysticalAdaptations {
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
-    }
-
-    @SubscribeEvent
-    public static void onServerStarted(ServerStartedEvent event) {
-        MinecraftServer server = event.getServer();
-        RecipeManager manager = server.getRecipeManager();
-
-        Recipe<?> orniumAxe = manager.byKey(ResourceLocation.fromNamespaceAndPath("mysticalextendedtier", "gear/ornium_axe")).orElse(null).value();
-        Recipe<?> orniumBoots = manager.byKey(ResourceLocation.fromNamespaceAndPath("mysticalextendedtier", "gear/ornium_boots")).orElse(null).value();
-        Recipe<?> orniumChestplate = manager.byKey(ResourceLocation.fromNamespaceAndPath("mysticalextendedtier", "gear/ornium_chestplate")).orElse(null).value();
-        Recipe<?> orniumHelmet = manager.byKey(ResourceLocation.fromNamespaceAndPath("mysticalextendedtier", "gear/ornium_helmet")).orElse(null).value();
-        Recipe<?> orniumHoe = manager.byKey(ResourceLocation.fromNamespaceAndPath("mysticalextendedtier", "gear/ornium_hoe")).orElse(null).value();
-        Recipe<?> orniumLeggings = manager.byKey(ResourceLocation.fromNamespaceAndPath("mysticalextendedtier", "gear/ornium_leggings")).orElse(null).value();
-        Recipe<?> orniumPickaxe = manager.byKey(ResourceLocation.fromNamespaceAndPath("mysticalextendedtier", "gear/ornium_pickaxe")).orElse(null).value();
-        Recipe<?> orniumShears = manager.byKey(ResourceLocation.fromNamespaceAndPath("mysticalextendedtier", "gear/ornium_shears")).orElse(null).value();
-        Recipe<?> orniumShovel = manager.byKey(ResourceLocation.fromNamespaceAndPath("mysticalextendedtier", "gear/ornium_shovel")).orElse(null).value();
-        Recipe<?> orniumSword = manager.byKey(ResourceLocation.fromNamespaceAndPath("mysticalextendedtier", "gear/ornium_sword")).orElse(null).value();
-        Recipe<?> orniumWateringCan = manager.byKey(ResourceLocation.fromNamespaceAndPath("mysticalextendedtier", "gear/ornium_watering_can")).orElse(null).value();
-
-        if (orniumAxe != null) {
-        }
-        if (orniumBoots != null) {
-        }
-        if (orniumChestplate != null) {
-        }
-        if (orniumHelmet != null) {
-        }
-        if (orniumHoe != null) {
-        }
-        if (orniumLeggings != null) {
-        }
-        if (orniumPickaxe != null) {
-        }
-        if (orniumShears != null) {
-        }
-        if (orniumShovel != null) {
-        }
-        if (orniumSword != null) {
-        }
-        if (orniumWateringCan != null) {
-        }
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
